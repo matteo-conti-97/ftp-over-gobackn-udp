7eeb5140dc71de2165ca2a1bb478fa3800aaea72
Matteo <matteo.conti.97@students.uniroma2.eu>
2020-09-09 00:09:53 +0200

    fork,sigchild
    Aggiunto il forking per gestire piu' utenti, la gestione di sigchild per non avere processi zombie e risolto problema della get, l'if va prima della send del comando

--- Client/client.c
+++ Client/client.c
@@ -7,79 +7,97 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-#define SERV_PORT 5192
+#define SERV_PORT 4444
 #define MAXLINE 1024
 #define PUT 1
 #define GET 2
 #define LIST 3
 #define EXIT 4
 
-void put(int sockfd){
-  int fd, n, command=1;
+void put(int sockfd);
+
+void get(int sockfd);
+
+void list(int sockfd);
+
+void choice(int sockfd);
+
+int main(int argc, char *argv[]){
+  int sockfd, fd, n, len, new_port;
   char buff[MAXLINE];
-  memset(buff,0,sizeof(buff));
-  /* Invia al server il pacchetto di richiesta*/
-  if (send(sockfd, &command, sizeof(int), 0) < 0) {
-    perror("errore in send");
-    exit(-1);
+  struct sockaddr_in servaddr, child_addr;
+  if (argc != 2) { /* controlla numero degli argomenti */
+    fprintf(stderr, "utilizzo: client <indirizzo IP server>\n");
+    exit(1);
   }
-  fd=open("imgPiccola.jpg",O_RDONLY);
-  while((n = read(fd, buff, MAXLINE))>0)
-  {
-    //usleep(1000);
-    if (send(sockfd, buff, n, 0) < 0)
-    {
-      perror("errore in send");
-      exit(-1);
-    }
-    memset(buff,0,sizeof(buff));
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { /* crea il socket */
+    perror("errore in socket");
+    exit(EXIT_FAILURE);
   }
-  printf("\nPUT terminata\n\n");
-  send(sockfd, "End", strlen("End"), 0);
-  close(fd);
+  //Buffer extension
+  /*int a = 65535;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &a, sizeof(int)) == -1) {
+    perror("errore resize buffer ricezione");
+  }
+  if (setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &a, sizeof(int)) == -1) {
+    perror("errore resize buffer ricezione");
+  }*/
+
+  memset((void *)&servaddr, 0, sizeof(servaddr));
+  memset((void *)&child_addr, 0, sizeof(child_addr));
+  /* azzera servaddr */
+  servaddr.sin_family = AF_INET;
+  /* assegna il tipo di indirizzo */
+  servaddr.sin_port = htons(SERV_PORT); /* assegna la porta del server */
+  /* assegna l'indirizzo del server prendendolo dalla riga di comando. L'indirizzo è
+  una stringa da convertire in intero secondo network byte order. */
+  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+  /* inet_pton (p=presentation) vale anche per indirizzi IPv6 */
+    perror("errore in inet_pton");
+    exit(EXIT_FAILURE);
+  }
+
+  /* stabilisce la connessione con il server */
+  if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+    perror("errore in connect");
+    exit(EXIT_FAILURE);
+  }
+
+ if (send(sockfd, NULL, 0, 0) < 0) {
+    perror("errore in send connessione con richiesta al server");
+    exit(EXIT_FAILURE);
+  }
+
+  if(recv(sockfd, &new_port, sizeof(int), 0)<0){
+    perror("errore in recv porta dedicata");
+    exit(EXIT_FAILURE);
+  }
+  /* azzera child_addr */
+  child_addr.sin_family = AF_INET;
+  /* assegna il tipo di indirizzo */
+  child_addr.sin_port = new_port; /* assegna la porta del figlio */
+  if (inet_pton(AF_INET, argv[1], &child_addr.sin_addr) <= 0) {
+  /* inet_pton (p=presentation) vale anche per indirizzi IPv6 */
+    perror("errore in inet_pton");
+    exit(EXIT_FAILURE);
+  }
+
+  /* stabilisce la connessione con il processo dedicato */
+  if (connect(sockfd, (struct sockaddr *) &child_addr, sizeof(child_addr)) < 0) {
+    perror("errore in connect");
+    exit(EXIT_FAILURE);
+  }
+  printf("Ti sei connesso con successo\n");
+
+  while(1){
+  choice(sockfd);
+  }
+  close(sockfd);
+  exit(EXIT_SUCCESS);
 }
 
-void get(int sockfd){
-  int n, fd, command=2;
-  char buff[MAXLINE];
-    /* Invia al server il pacchetto di richiesta*/
-  if (send(sockfd, &command, sizeof(int), 0) < 0) {
-    perror("errore in send");
-    exit(-1);
-  }
-  //apro file
-  fd = open("prova1.jpg", O_RDWR | O_CREAT| O_TRUNC, 0666);
 
-   while((n = recv(sockfd, buff, MAXLINE, 0))) {
-        buff[n] = 0;
-        if (!(strcmp(buff, "End"))) {
-            break;
-        }
-        write(fd, buff, n);
-    }
-  printf("\nGET terminata\n\n");
-  close(fd);
-}
 
-void list(int sockfd){
-  int n, fd, command=3;
-  char buff[MAXLINE];
-    /* Invia al server il pacchetto di richiesta*/
-  if (send(sockfd, &command, sizeof(int), 0) < 0) {
-    perror("errore in send");
-    exit(-1);
-  }
-  printf("Lista dei file su server:\n\n");
-  //listo file
-   while((n = recv(sockfd, buff, MAXLINE, 0))) {
-        buff[n] = 0;
-        if (!(strcmp(buff, "End"))) {
-            break;
-        }
-        printf("%s\n", buff);
-    }
-  printf("\nLIST terminata\n\n");
-}
 
 void choice(int sockfd){
   int n;
@@ -96,21 +114,22 @@ void choice(int sockfd){
   }
   switch(n){
     case PUT:
-      system("clear");
+      //system("clear");
       put(sockfd);
       break;
     case GET:
-      system("clear");
+      //system("clear");
       get(sockfd);
       break;
     case LIST:
-      system("clear");
+      //system("clear");
       list(sockfd);
       break;
     case EXIT:
-      system("clear");
+      //system("clear");
       close(sockfd);
-      exit(0);
+      exit(EXIT_SUCCESS);
+      break;
     default:
       printf("Inserisci un numero valido\n");
       goto selectOpt;
@@ -118,48 +137,81 @@ void choice(int sockfd){
   }
 }
 
-int main(int argc, char *argv[ ]){
-  int sockfd, fd, n, len;
+void list(int sockfd){
+  int n, fd, command=3;
   char buff[MAXLINE];
-  struct sockaddr_in servaddr;
-  if (argc != 2) { /* controlla numero degli argomenti */
-    fprintf(stderr, "utilizzo: daytime_clientUDP <indirizzo IP server>\n");
-    exit(1);
+    /* Invia al server il pacchetto di richiesta*/
+  if (send(sockfd, &command, sizeof(int), 0) < 0) {
+    perror("errore in send");
+    exit(EXIT_FAILURE);
   }
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { /* crea il socket */
-    perror("errore in socket");
-    exit(-1);
-  }
-  //Buffer extension
-  /*int a = 65535;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &a, sizeof(int)) == -1) {
-    perror("errore resize buffer ricezione");
-  }
-  if (setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &a, sizeof(int)) == -1) {
-    perror("errore resize buffer ricezione");
-  }*/
+  printf("Lista dei file su server:\n\n");
+  //listo file
+   while((n = recv(sockfd, buff, MAXLINE, 0))) {
+        buff[n] = 0;
+        if (!(strcmp(buff, "End"))) {
+            break;
+        }
+        printf("%s\n", buff);
+    }
+  printf("\nLIST terminata\n\n");
+}
 
-  memset((void *)&servaddr, 0, sizeof(servaddr));
-  /* azzera servaddr */
-  servaddr.sin_family = AF_INET;
-  /* assegna il tipo di indirizzo */
-  servaddr.sin_port = htons(SERV_PORT); /* assegna la porta del server */
-  /* assegna l'indirizzo del server prendendolo dalla riga di comando. L'indirizzo è
-  una stringa da convertire in intero secondo network byte order. */
-  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-  /* inet_pton (p=presentation) vale anche per indirizzi IPv6 */
-    perror("errore in inet_pton");
+void get(int sockfd){
+  int n, fd, command=2;
+  char buff[MAXLINE];
+
+    //apro file
+  if((fd = open("prova1.jpg", O_RDWR | O_CREAT| O_TRUNC, 0666))<0){
+    perror("errore apertura/creazione file da ricevere");
+    exit(EXIT_FAILURE);
+  }
+
+    /* Invia al server il pacchetto di richiesta*/
+  if (send(sockfd, &command, sizeof(int), 0) < 0) {
+    perror("errore in send");
     exit(-1);
   }
-  /* stabilisce la connessione con il server */
-  if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-    perror("errore in connect");
-    exit(-1);
+
+   while((n = recv(sockfd, buff, MAXLINE, 0))) {
+        buff[n] = 0;
+        if (!(strcmp(buff, "End"))) {
+            break;
+        }
+        write(fd, buff, n);
+    }
+  printf("\nGET terminata\n\n");
+  close(fd);
+}
+
+void put(int sockfd){
+  int fd, n, command=1;
+  char buff[MAXLINE];
+  memset(buff,0,sizeof(buff));
+  
+  // apro file
+  if((fd=open("imgPiccola.jpg",O_RDONLY))<0){
+    perror("errore apertura file da inviare");
+    exit(EXIT_FAILURE);
   }
-  printf("Ti sei connesso con successo al server\n");
-  while(1){
-  choice(sockfd);
+
+  /* Invia al server il pacchetto di richiesta*/
+  if (send(sockfd, &command, sizeof(int), 0) < 0) {
+    perror("errore in send");
+    exit(EXIT_FAILURE);
   }
-  close(sockfd);
-  exit(0);
+
+  while((n = read(fd, buff, MAXLINE))>0)
+  {
+    //usleep(1000);
+    if (send(sockfd, buff, n, 0) < 0)
+    {
+      perror("errore in send");
+      exit(EXIT_FAILURE);
+    }
+    memset(buff,0,sizeof(buff));
+  }
+  printf("\nPUT terminata\n\n");
+  send(sockfd, "End", strlen("End"), 0);
+  close(fd);
 }
--- Server/server.c
+++ Server/server.c
@@ -8,24 +8,178 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <dirent.h> 
-#define SERV_PORT 5192
+#include <signal.h>
+#include <sys/wait.h>
+
+#define SERV_PORT 4444
 #define MAXLINE 1024
 #define PUT 1
 #define GET 2
 #define LIST 3
+#define EXIT 4
+
+typedef void Sigfunc(int); 
+
+Sigfunc* signal(int signum, Sigfunc *handler);
+
+void sig_child_handler(int signum);
+
+void get(int sockfd, struct sockaddr_in addr);
+
+void put(int sockfd, struct sockaddr_in addr);
+
+void list(int sockfd, struct sockaddr_in addr);
+
+
+int main(int argc, char *argv[ ]){
+  int sockfd, child_sockfd, len, child_len, command, child_port;
+  struct sockaddr_in addr, child_addr;
+  pid_t pid;
+
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { /* crea il socket */
+    perror("errore in socket padre");
+    exit(EXIT_FAILURE);
+  }
+  //Buffer extension
+  /*int a = 65535;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &a, sizeof(int)) == -1) {
+    perror("errore resize buffer ricezione");
+  }
+  if (setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &a, sizeof(int)) == -1) {
+    perror("errore resize buffer ricezione");
+  }*/
+  memset((void *)&addr, 0, sizeof(addr));
+  memset((void *)&child_addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY); /* il server accetta pacchetti su una qualunque delle sue interfacce di rete */
+  addr.sin_port = htons(SERV_PORT); /* numero di porta del server */
+  len = sizeof(addr);
+
+  /* assegna l'indirizzo al socket */
+  if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+    perror("errore in bind");
+    exit(EXIT_FAILURE);
+  }
+
+  if (signal(SIGCHLD, sig_child_handler) == SIG_ERR) { 
+    fprintf(stderr, "errore in signal");
+    exit(1);
+  }
+
+  while (1) {
+    //Ascolto di richieste di connessione dei client
+    if ((recvfrom(sockfd, &command, sizeof(int), 0, (struct sockaddr *)&addr, &len)) < 0) {
+      perror("errore in recvfrom");
+      exit(EXIT_FAILURE);
+    }
+
+    //Creo il socket del figlio dedicato al client
+    if ((child_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
+    perror("errore in socket figlio");
+    exit(EXIT_FAILURE);
+    }
+
+    child_addr.sin_family = AF_INET;
+    child_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* il figlio accetta pacchetti su una qualunque delle sue interfacce di rete */
+    child_addr.sin_port = htons(0);
+    child_len=sizeof(child_addr);
+
+    /* assegna l'indirizzo al socket del figlio */
+    if (bind(child_sockfd, (struct sockaddr *) &child_addr, sizeof(child_addr)) < 0) {
+    perror("errore in bind");
+    exit(EXIT_FAILURE);
+    }
+
+    //Prendo il numero di porta del figlio
+    if(getsockname(child_sockfd, (struct sockaddr *) &child_addr, &child_len)<0){
+      perror("errore acquisizione numero porta del socket processo figlio");
+      exit(EXIT_FAILURE);
+    }
+    child_port=child_addr.sin_port;
+    //Invio la nuova porta al client
+    if(sendto(sockfd, &child_port, sizeof(int), 0, (struct sockaddr *)&addr, sizeof(addr))<0){
+      perror("errore in sendto porta figlio");
+      exit(EXIT_FAILURE);
+    }
+
+    if ((pid = fork()) == 0){
+      printf("Sono nel figlio\n");
+      while(1){
+        if ((recvfrom(child_sockfd, &command, sizeof(int), 0, (struct sockaddr *)&child_addr, &child_len)) < 0) {
+          perror("errore in recvfrom");
+          exit(EXIT_FAILURE);
+        }
+
+        switch(command){
+          case PUT:
+            //printf("Ho ricevuto il comando put %d\n",command);
+            put(child_sockfd, child_addr);
+            break;
+          case GET:
+            //printf("Ho ricevuto il comando get %d\n",command);
+            get(child_sockfd, child_addr);
+            break;
+          case LIST:
+            //printf("Ho ricevuto il comando list %d\n",command);
+            list(child_sockfd, child_addr);
+            break;
+          case EXIT:
+            close(child_sockfd);
+            exit(EXIT_SUCCESS);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+ 
+   exit(EXIT_SUCCESS);
+}
+
+
+
+
+Sigfunc *signal(int signum, Sigfunc *func){
+  struct sigaction  act, oact;
+    /* la struttura sigaction memorizza informazioni riguardanti la
+  manipolazione del segnale */
+
+  act.sa_handler = func;
+  sigemptyset(&act.sa_mask);  /* non occorre bloccare nessun altro segnale */
+  act.sa_flags = 0;
+  if (signum != SIGALRM) 
+     act.sa_flags |= SA_RESTART;  
+  if (sigaction(signum, &act, &oact) < 0)
+    return(SIG_ERR);
+  return(oact.sa_handler);
+}
+
+void sig_child_handler(int signum){
+  int status;
+  pid_t pid;
+  while((pid = waitpid(WAIT_ANY, &status, WNOHANG)>0))
+    printf("Figlio %d terminato",pid);
+  return;
+}
 
 void get(int sockfd, struct sockaddr_in addr){
   int fd,n;
   char buff[MAXLINE];
   memset(buff,0,sizeof(buff));
-  fd=open("imgPiccola.jpg",O_RDONLY);
+
+  if((fd=open("./files/imgPiccola.jpg",O_RDONLY))<0){
+    perror("errore apertura file da inviare");
+    exit(EXIT_FAILURE);
+  }
+
   while((n = read(fd, buff, MAXLINE))>0)
   {
     //usleep(1000);
     if (sendto(sockfd, buff, n, 0, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
       perror("errore in sendto");
-      exit(-1);
+      exit(EXIT_FAILURE);
     }
     memset(buff,0,sizeof(buff));
   }
@@ -39,7 +193,10 @@ void put(int sockfd, struct sockaddr_in addr){
   char buff[MAXLINE];
   len=sizeof(addr);
   //apro file
-  fd = open("files/prova1.jpg", O_RDWR | O_CREAT| O_TRUNC, 0666);
+  if((fd = open("files/prova1.jpg", O_RDWR | O_CREAT| O_TRUNC, 0666))<0){
+    perror("errore apertura/creazione file da ricevere");
+    exit(EXIT_FAILURE);
+  }
 
    while((n = recvfrom(sockfd, buff, MAXLINE, 0, (struct sockaddr *) &addr, &len))) {
         buff[n] = 0;
@@ -56,73 +213,20 @@ void list(int sockfd, struct sockaddr_in addr){
   DIR *d;
   struct dirent *dir;
   int n;
-  d = opendir("./files");
+  if((d = opendir("./files"))==NULL){
+    perror("errore apertura directory dei file");
+    exit(EXIT_FAILURE);
+  }
   while ((dir = readdir(d)) != NULL) {
     if((strcmp(dir->d_name,".")==0)||(strcmp(dir->d_name,"..")==0)) continue;
     if (sendto(sockfd, &(dir->d_name), sizeof(dir->d_name), 0, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
       perror("errore in sendto");
-      exit(-1);
+      exit(EXIT_FAILURE);
     }
   }
   printf("Ho finito di inviare\n");
   sendto(sockfd, "End", strlen("End"), 0, (struct sockaddr *) &addr, sizeof(addr));
   closedir(d);
 }
-
-int main(int argc, char *argv[ ]){
-  int sockfd, len, command;
-  struct sockaddr_in addr;
-  unsigned char buff[MAXLINE];
-
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { /* crea il socket */
-    perror("errore in socket");
-    exit(-1);
-  }
-  //Buffer extension
-  /*int a = 65535;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &a, sizeof(int)) == -1) {
-    perror("errore resize buffer ricezione");
-  }
-  if (setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &a, sizeof(int)) == -1) {
-    perror("errore resize buffer ricezione");
-  }*/
-  memset((void *)&addr, 0, sizeof(addr));
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY); /* il server accetta pacchetti su
-  una qualunque delle sue interfacce di rete */
-  addr.sin_port = htons(SERV_PORT); /* numero di porta del server */
-  
-  /* assegna l'indirizzo al socket */
-  if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-    perror("errore in bind");
-    exit(-1);
-  }
-
-  while (1) {
-    len = sizeof(addr);
-    if ((recvfrom(sockfd, &command, sizeof(int), 0, (struct sockaddr *)&addr, &len)) < 0) {
-      perror("errore in recvfrom");
-      exit(-1);
-    }
-  
-  switch(command){
-    case PUT:
-      put(sockfd, addr);
-      break;
-    case GET:
-      get(sockfd, addr);
-      break;
-    case LIST:
-      list(sockfd, addr);
-      break;
-    default:
-      break;
-  }
- 
-  }
-   exit(0);
-}
-
-
     
